0e68ea37978c10ae050bd0a28c54da848588d3c5
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-05-21 16:50:07 +0600

    Remove not needed aysnc and await to attempt to  solve vercel data not showing time to time issue

--- index.js
+++ index.js
@@ -32,120 +32,102 @@ const client = new MongoClient(uri, {
     },
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    maxPoolSize:10,
+    maxPoolSize: 10,
 });
 
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        // client.connect();
-
-        const toys = client.db("whiskAndPlayDB").collection("toys");
 
 
-        const indexKeys = { ToyName: 1 };
-        const indexOptions = { name: "toyName" };
-        const result = await toys.createIndex(indexKeys, indexOptions);
+const toys = client.db("whiskAndPlayDB").collection("toys");
 
-
-        app.get('/toys', async (req, res) => {
-            const category = req.query.category;
-            if (category === "Baking Kits" || category === "Food Prep Tools" || category === "Utensils") {
-                const result = await toys.find({ category: category }).limit(8).toArray();
-                return res.send(result);
-            }
-            const result = await toys.find().toArray();
-            res.send(result)
-        })
-
-        app.get('/toys/:id', async (req, res) => {
-            const id = req.params.id;
-            const result = await toys.find({ _id: new ObjectId(id) }).toArray();
-            res.send(result);
-        })
-
-        app.post('/toys', async (req, res) => {
-            const newToy = req.body;
-            const result = await toys.insertOne(newToy);
-            res.send(result)
-        })
-
-        app.delete('/toys/:id', async (req, res) => {
-            const id = req.params.id;
-            const result = await toys.deleteOne({ _id: new ObjectId(id) });
-            res.send(result)
-        })
-
-        app.patch('/toys/:id', async (req, res) => {
-            const id = req.params.id;
-            const body = req.body;
-            const updatedToy = {
-                $set: {
-                    ...body
-                }
-            }
-            const result = await toys.updateOne({ _id: new ObjectId(id) }, updatedToy);
-            res.send(result)
-        })
-
-
-        app.get('/all-toys', async (req, res) => {
-            const limit = parseInt(req.query.limit) || 20;
-
-            const searchText = req.query.search;            
-            if (searchText) {
-                const result = await toys.find({ ToyName: { $regex: searchText, $options: 'i' } }).limit(limit).toArray();
-                return res.send(result);
-            }
-
-            const sort = req.query.sort;
-            if (sort) {
-                if (sort === "asc") {
-                    const result = await toys.find().sort({ "price": 1 }).limit(limit).toArray();
-                    return res.send(result);
-                }
-
-                if (sort === "desc") {
-                    const result = await toys.find().sort({ "price": -1 }).limit(limit).toArray();
-                    return res.send(result);
-                }
-            }
-
-            const result = await toys.find().limit(limit).toArray();
-            res.send(result);
-        })
-
-        app.get('/my-toys/:email', async (req, res) => {
-            const email = req.params.email;
-
-            const sort = req.query.sort;
-            if (sort) {
-                if (sort === "asc") {
-                    const result = await toys.find({ sellerEmail: email }).sort({ "price": 1 }).toArray();
-                    return res.send(result);
-                }
-
-                if (sort === "desc") {
-                    const result = await toys.find({ sellerEmail: email }).sort({ "price": -1 }).toArray();
-                    return res.send(result);
-                }
-            }
-
-            const result = await toys.find({ sellerEmail: email }).toArray();
-            res.send(result)
-        })
-
-
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+app.get('/toys', async (req, res) => {
+    const category = req.query.category;
+    if (category === "Baking Kits" || category === "Food Prep Tools" || category === "Utensils") {
+        const result = await toys.find({ category: category }).limit(8).toArray();
+        return res.send(result);
     }
-}
-run().catch(console.dir);
+    const result = await toys.find().toArray();
+    res.send(result)
+})
 
+app.get('/toys/:id', async (req, res) => {
+    const id = req.params.id;
+    const result = await toys.find({ _id: new ObjectId(id) }).toArray();
+    res.send(result);
+})
+
+app.post('/toys', async (req, res) => {
+    const newToy = req.body;
+    const result = await toys.insertOne(newToy);
+    res.send(result)
+})
+
+app.delete('/toys/:id', async (req, res) => {
+    const id = req.params.id;
+    const result = await toys.deleteOne({ _id: new ObjectId(id) });
+    res.send(result)
+})
+
+app.patch('/toys/:id', async (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    const updatedToy = {
+        $set: {
+            ...body
+        }
+    }
+    const result = await toys.updateOne({ _id: new ObjectId(id) }, updatedToy);
+    res.send(result)
+})
+
+
+app.get('/all-toys', async (req, res) => {
+    const limit = parseInt(req.query.limit) || 20;
+
+    const searchText = req.query.search;
+    if (searchText) {
+        const result = await toys.find({ ToyName: { $regex: searchText, $options: 'i' } }).limit(limit).toArray();
+        return res.send(result);
+    }
+
+    const sort = req.query.sort;
+    if (sort) {
+        if (sort === "asc") {
+            const result = await toys.find().sort({ "price": 1 }).limit(limit).toArray();
+            return res.send(result);
+        }
+
+        if (sort === "desc") {
+            const result = await toys.find().sort({ "price": -1 }).limit(limit).toArray();
+            return res.send(result);
+        }
+    }
+
+    const result = await toys.find().limit(limit).toArray();
+    res.send(result);
+})
+
+app.get('/my-toys/:email', async (req, res) => {
+    const email = req.params.email;
+
+    const sort = req.query.sort;
+    if (sort) {
+        if (sort === "asc") {
+            const result = await toys.find({ sellerEmail: email }).sort({ "price": 1 }).toArray();
+            return res.send(result);
+        }
+
+        if (sort === "desc") {
+            const result = await toys.find({ sellerEmail: email }).sort({ "price": -1 }).toArray();
+            return res.send(result);
+        }
+    }
+
+    const result = await toys.find({ sellerEmail: email }).toArray();
+    res.send(result)
+})
+
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send({ message: "Whisk & Play server is running" });
